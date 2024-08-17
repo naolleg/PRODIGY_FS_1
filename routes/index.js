cb455f1f229780d98e3e0af94404416391c0316a
naolleg <naollegesse51@gmail.com>
2024-08-17 22:57:44 +0300

    reconfigured the react with tailwindcss

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ const appRoute = express.Router();
 
 // all routes
 import userRoutes from "./user.route.js";
-import loginRoutes from "./login.route.js";
+//import loginRoutes from "./login.route.js";
 
 // adding middleware
 appRoute.use(userRoutes);
-appRoute.use(loginRoutes);
+//appRoute.use(loginRoutes);
 
 
 export default appRoute;
